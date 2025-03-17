bf729eed4768872ae50168ff50af592faaa12f3f
LimDong <50188317+Limdongdang@users.noreply.github.com>
2025-03-17 10:25:11 +0900

    주석 제거

--- cardgame_project-main2/src/api/rank.php
+++ cardgame_project-main2/src/api/rank.php
@@ -2,20 +2,6 @@
 header("Content-Type: application/json; charset=UTF-8");
 require 'db.php';
 
-// 데이터베이스 연결 정보
-//$servername = "localhost";
-//$username = "root";
-//$password = "jungwon04^^";
-//$dbname = "n";
-
-// MySQL 연결
-//$conn = new mysqli($servername, $username, $password, $dbname);
-
-// 연결 확인
-//if ($conn->connect_error) {
-//    die(json_encode(["status" => "error", "message" => "DB 연결 실패"]));
-//}
-
 // 랭킹 조회 쿼리 (점수 내림차순 정렬)
 $sql = "SELECT nickname, score FROM users ORDER BY score DESC";
 $result = $conn->query($sql);
--- cardgame_project-main2/src/api/result.php
+++ cardgame_project-main2/src/api/result.php
@@ -10,35 +10,29 @@ if($nickname === null || $score === null) {
 	exit();
 }
 
-  //DB연결
-  //$dbcon = mysqli_connect('localhost', 'root','jungwon04^^', 'n');
-  
+//DB 쿼리
+$query = "SELECT * FROM users WHERE nickname = '$nickname'";
+// 실행
+$result = mysqli_query($conn, $query);
 
- //DB 쿼리
+// 결과 확인
+if (mysqli_num_rows($result) > 0) {
+  echo json_encode(["status" => "error", "message" => "동일한 닉네임이 존재합니다."]);
+  exit();
+} else {
+  //닉네임 중복X
+  $query = "insert into users(nickname,score) values ('$nickname', '$score')";
 
- $query = "SELECT * FROM users WHERE nickname = '$nickname'";
-//  $query = "insert into cardgame values ('$nickname')";
-  // 실행
-  $result = mysqli_query($conn, $query);
-
-  // 결과 확인
-  if (mysqli_num_rows($result) > 0) {
-    echo json_encode(["status" => "error", "message" => "동일한 닉네임이 존재합니다."]);
+  if (mysqli_query($conn, $query)) {
+    echo json_encode(["status" => "success", "message" => "사용자가 성공적으로 등록되었습니다."]);
     exit();
   } else {
-    //닉네임 중복X
-    $query = "insert into users(nickname,score) values ('$nickname', '$score')";
-
-    if (mysqli_query($conn, $query)) {
-      echo json_encode(["status" => "success", "message" => "사용자가 성공적으로 등록되었습니다."]);
-      exit();
-    } else {
-      echo json_encode(["status" => "error", "message" => "사용자 등록 실패", "error" => mysqli_error($conn)]);
-      exit();
-    }
+    echo json_encode(["status" => "error", "message" => "사용자 등록 실패", "error" => mysqli_error($conn)]);
+    exit();
   }
+}
 
-  //DB연결 종료
-  mysqli_close($conn);
-  // echo "PHP 실행 확인";
+//DB연결 종료
+mysqli_close($conn);
+// echo "PHP 실행 확인";
 ?>
