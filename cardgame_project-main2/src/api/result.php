dcb4c8a817fd595f3d2067195b903c76653b53d3
LimDong <50188317+Limdongdang@users.noreply.github.com>
2025-03-17 23:05:11 +0900

    result.php 업데이트

--- cardgame_project-main2/src/api/result.php
+++ cardgame_project-main2/src/api/result.php
@@ -1,38 +1,39 @@
 <?php
-require 'db.php';
-header("Content-Type: application/json; charset=UTF-8");
+  
+  $nickname = $_POST['nickname'];
+  // $score = $_POST['score'];
 
-$nickname = isset($_POST['nickname']) ? $_POST['nickname'] : null;
-$score = isset($_POST['score']) ? $_POST['score'] : null;
+  //DB연결
+  require 'db.php';
+  
 
-if($nickname === null || $score === null) {
-	echo json_encode(["status" => "error", "message" => "닉네임과 점수는 필수 항목입니다."]);
-	exit();
-}
+  //DB 쿼리
 
-//DB 쿼리
-$query = "SELECT * FROM users WHERE nickname = '$nickname'";
-// 실행
-$result = mysqli_query($conn, $query);
+  $query = "SELECT * FROM users WHERE 닉네임 = '$nickname'";
+  //  $query = "insert into cardgame values ('$nickname')";
+  // 실행
+  $result = mysqli_query($dbcon, $query);
 
-// 결과 확인
-if (mysqli_num_rows($result) > 0) {
-  echo json_encode(["status" => "error", "message" => "동일한 닉네임이 존재합니다."]);
-  exit();
-} else {
-  //닉네임 중복X
-  $query = "insert into users(nickname,score) values ('$nickname', '$score')";
-
-  if (mysqli_query($conn, $query)) {
-    echo json_encode(["status" => "success", "message" => "사용자가 성공적으로 등록되었습니다."]);
-    exit();
+  // 결과 확인
+  if (mysqli_num_rows($result) > 0) {
+    //닉네임 중복O
+    //   header("Location: MarsProject.html");
+    // echo "<script>alert('동일한 닉네임이 존재합니다.')</script>";
+    echo "<script>
+              alert('동일한 닉네임이 존재합니다.');
+            </script>";
+      exit();
   } else {
-    echo json_encode(["status" => "error", "message" => "사용자 등록 실패", "error" => mysqli_error($conn)]);
-    exit();
-  }
-}
+    //닉네임 중복X
+    $query = "insert into users(닉네임,점수) values ('$nickname',0)";
 
-//DB연결 종료
-mysqli_close($conn);
-// echo "PHP 실행 확인";
+    if (mysqli_query($dbcon, $query)) {
+      exit();
+  }
+    // header("Location: GameScreen.html");
+  }
+
+  //DB연결 종료
+  mysqli_close($dbcon);
+  // echo "PHP 실행 확인";
 ?>
